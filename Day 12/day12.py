07c02d7891fe0c4bb62a70c4baa75738efa90e26
MarvelousBilly <jaxmac101@gmail.com>
2025-12-12 13:01:37 -0500

    Made my day 12 a one-liner because i felt like it. Also made the boilerplate easier to work with and made it look nicer in the console.

--- Day 12/day12.py
+++ Day 12/day12.py
@@ -1,52 +1,26 @@
-import math
 from stopwatch import Stopwatch
-from functools import lru_cache
-from tqdm import tqdm
-    
-def generateGraph(f):
-    stopwatch = Stopwatch()
-    stopwatch.start()
-    data = [[l.strip().split(": ")[0], l.strip().split(": ")[1].split(" ")] for l in f]
-    for d in data:
-        graphDict.setdefault(d[0], graph(d[0])).add(d[1])
-    stopwatch.stop()
-    print(stopwatch.report())
-    
-        
+from math import prod as p
+from itertools import islice as i
+from re import split as s
+
 def part1(f):
-    boxCount = [0] * 6
-    lines = [l.strip() for l in f]
-    
-    cnt = 0
-    for ln, line in enumerate(lines):
-        if ln < 30:
-            if(ln % 5 == 4):
-                box = "".join(lines[(ln // 5) * 5:((ln+1) // 5) * 5]) #could've hardcoded this but i'm this far in you think im hardcoding (aside from the linecounts ignore those)
-                boxCount[ln // 5] = box.count("#")
-                
-        else: #how tall the boxes are in inputs
-            size = int(line.split(":")[0].split("x")[0]) * int(line.split(":")[0].split("x")[1]) #what on EARTH is this btw. wrote it to just see if the idea was right (it was) but damn this sucks
-            boxes = line.split(": ")[1].strip().split(" ") #barf
+    #Funny teehee oneliner because i was bored
+    return sum(p(map(int, l[0:2])) >= 9 * sum(map(int, l[3:])) for l in (s("\D", c.strip()) for c in i(f,30,None)))
 
-            total = 0
-            for i, num in enumerate(boxes):
-                total += boxCount[i] * int(num)
-                
-            if(size >= total):
-                cnt += 1
-    
-    
-    return cnt
+def part2(f):
+    return "Advent of Code 2025 Complete!"
 
-def run(part, f, example, exNum, ptNum):
-    stopwatch = Stopwatch()
-    stopwatch.start()
+def run(part, f, partNumber, example, exNum, ptNum):
+    sw = Stopwatch()
+    sw.start()
     num = part(f)
-    stopwatch.stop()
-    print(num)
-    print(stopwatch.report())
+    sw.stop()
+    print(f"Part #{partNumber}: {num}", end = " ")
+    print(f"[{sw.elapsed:.2f}s]" if sw.elapsed > 0.1 else f"[{sw.elapsed * 1000:.2f}ms]")
     assert(num == (exNum if example else ptNum))
+    f.seek(0)
 
 example = False
 with open("example.txt" if example else "input.txt") as f:
-    run(part1, f, example, 2, 546) #lmao this literally doesn't work for the example the damn troll
+    run(part1, f, 1, example, 2, 546) #lmao this literally doesn't work for the example the damn troll
+    run(part2, f, 2, example, 2, "Advent of Code 2025 Complete!")
